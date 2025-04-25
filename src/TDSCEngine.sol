05879ae00d5854049c484267b6a4d8c26332bb5f
SurajAnand88 <surajbloodstone@gmail.com>
2025-04-25 13:28:23 +0530

    created mint function

--- src/TDSCEngine.sol
+++ src/TDSCEngine.sol
@@ -5,6 +5,7 @@
 // imports
 // interfaces, libraried , contracts
 // errors
+//State Variables
 // Events
 // Modifiers
 // Functions
@@ -46,26 +47,36 @@ pragma solidity 0.8.18;
 
 import {DecentralizedStableCoin} from "src/DecentralizedStablecoin.sol";
 import {ReentrancyGuard} from "../lib/openzepplin-contracts/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "../lib/openzepplin-contracts/contracts/token/ERC20/IERC20.sol";
+import {AggregatorV3Interface} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract TDSCEngine is ReentrancyGuard {
     /*═══════════════════════════════════════
                 Errors
-═══════════════════════════════════════*/
+    ═══════════════════════════════════════*/
 
     error TDSCEngine__AmountMustBeGreaterThanZero();
     error TDSCEngine__TokenAndPriceFeedArrayLengthMustBeEqual();
-    error TDSC__TokenNotAllowed();
-
+    error TDSCEngine__TokenNotAllowed();
+    error TDSCEngine_CollateralTransferFailed();
     /*═══════════════════════════════════════
                 State Variables
-═══════════════════════════════════════*/
-
+    ═══════════════════════════════════════*/
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant PRECISION = 1e18;
     mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 collateral)) private s_usersCollateralDeposit;
+    mapping(address user => uint256 tdsc) private s_UsersTDSCBalance;
+    address[] private s_collateralTokens;
     DecentralizedStableCoin private immutable i_Tdsc;
 
     /*═══════════════════════════════════════
+                Events
+    ═══════════════════════════════════════*/
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+    /*═══════════════════════════════════════
                 Modifiers
-═══════════════════════════════════════*/
+    ═══════════════════════════════════════*/
 
     modifier moreThanZero(uint256 amount) {
         if (amount == 0) revert TDSCEngine__AmountMustBeGreaterThanZero();
@@ -73,31 +84,27 @@ contract TDSCEngine is ReentrancyGuard {
     }
 
     modifier isAllowedToken(address tokenAddress) {
-        if (s_priceFeeds[tokenAddress] == address(0))
-            revert TDSC__TokenNotAllowed();
+        if (s_priceFeeds[tokenAddress] == address(0)) {
+            revert TDSCEngine__TokenNotAllowed();
+        }
         _;
     }
 
     /*═══════════════════════════════════════ 
                 Functions
-═══════════════════════════════════════*/
+    ═══════════════════════════════════════*/
 
-    /*═══════════════════════════════════
-    ════ 
+    /*═══════════════════════════════════════ 
                 External Functions
+    ═══════════════════════════════════════*/
 
-═══════════════════════════════════════*/
-
-    constructor(
-        address[] memory tokenAddresses,
-        address[] memory priceFeedAddresses,
-        address TDSCAddress
-    ) {
-        if (tokenAddresses.length != priceFeedAddresses.length)
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address TDSCAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
             revert TDSCEngine__TokenAndPriceFeedArrayLengthMustBeEqual();
-
+        }
         for (uint256 i = 0; i < tokenAddresses.length; i++) {
             s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+            s_collateralTokens.push(tokenAddresses[i]);
         }
         i_Tdsc = DecentralizedStableCoin(TDSCAddress);
     }
@@ -105,32 +112,97 @@ contract TDSCEngine is ReentrancyGuard {
     function depositeCollateralAndMintTDSC() external {}
 
     /*
-
-     *  @param tokenCollateralAddress the
-      address of the token to deposite as collateral
-     * @param amountCollateral the amount
-     *  of collateral to deposite
+     *  @notice follow the CEI pattern 
+     *  @param tokenCollateralAddress the address of the token to deposite as collateral
+     * @param amountCollateral the amount of collateral to deposite
      */
 
-    function depositeCollateral(
-        address tokenCollateralAddress,
-        uint256 amountCollateral
-    )
+    function depositeCollateral(address tokenCollateralAddress, uint256 amountCollateral)
         external
         moreThanZero(amountCollateral)
         isAllowedToken(tokenCollateralAddress)
         nonReentrant
-    {}
+    {
+        s_usersCollateralDeposit[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) revert TDSCEngine_CollateralTransferFailed();
+    }
 
     function redeemCollateralForTDSC() external {}
 
     function redeemCollateral() external {}
 
-    function mintTDSC() external {}
+    /*
+     *  @notice follow the CEI pattern 
+     *  @param amountTDSCtoMint the amount of decentralized stablecoin to mint 
+     * @param user must have more collateral value than minimun threshold
+     */
+
+    function mintTDSC(uint256 amountTDSCtoMint) external moreThanZero(amountTDSCtoMint) nonReentrant {
+        s_UsersTDSCBalance[msg.sender] += amountTDSCtoMint;
+    }
 
     function withdrawCollateralForTDSC() external {}
 
     function burnTDSC() external {}
 
     function liquidate() external {}
+
+    /*═══════════════════════════════════════ 
+        Private and Internal Functions
+    ═══════════════════════════════════════*/
+    /**
+     *
+     * @return totalTDSCMinted by user
+     * @return collaterAmountInUSD deposited by user
+     */
+    function _getAccountInformation(address user)
+        private
+        view
+        returns (uint256 totalTDSCMinted, uint256 collaterAmountInUSD)
+    {
+        totalTDSCMinted = s_UsersTDSCBalance[user];
+        collaterAmountInUSD = getUserCollaterAmountInUSD(user);
+    }
+    /**
+     * Returns how close to liquidation a user is
+     * If the user is below 1, they can get liquidated
+     * Check the ratio  of collateral to TDSC minted to get the health factor
+     */
+
+    function _healthFactor(address user) private view {
+        // Get Total TDSC minted
+        // Get total Collaterla depostied in USD
+        (uint256 totalTDSCMinted, uint256 totalCollaterlaValueInUSD) = _getAccountInformation(user);
+    }
+
+    function _revertHealthFactor(address user) internal view {
+        // Check health factor (Do user have enough collateral)
+        // Revert if thery don't
+        _healthFactor(user);
+    }
+
+    /*═══════════════════════════════════════ 
+        Public and External view Functions
+    ═══════════════════════════════════════*/
+
+    /**
+     *
+     * @return totalCollaterlAmountInUSD deposited by user
+     */
+    function getUserCollaterAmountInUSD(address user) public view returns (uint256 totalCollaterlAmountInUSD) {
+        for(uint256 i=0;i<s_collateralTokens.length;i++){
+            address token = s_collateralTokens[i];
+            uint256 amount  = s_usersCollateralDeposit[user][token];
+            totalCollaterlAmountInUSD += getUSDValue(token,amount);
+        }
+    }
+
+    function getUSDValue(address token, uint256 amount) public view returns (uint256){
+        //get the price feed
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return ((uint256(price)*ADDITIONAL_FEED_PRECISION) * amount)/PRECISION;
+    }
 }
